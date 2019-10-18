079b926378d943d1d2bb8e78e4f246dc3b7363b6
cortex <ingdiego.corts65@gmail.com>
2019-10-18 03:48:20 -0500

    Adjust sanitize url and search videos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {Component} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {YoutubeService} from './youtube.service';
+import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,56 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'wesura';
+  videos: any[];
+  selectVideo: {
+    videoId: '',
+    description: '',
+    title: '',
+    thumbnails: '',
+    urlEmbed: any
+  };
+  search = '';
+
+  constructor(private spinner: NgxSpinnerService, private youTubeService: YoutubeService, private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit () {
+    this.spinner.show();
+    this.videos = [];
+    this.youTubeService.getVideos(6).subscribe(lista => {
+      lista['items'].map(item => {
+        this.videos.push(this.formatDataVideo(item));
+      });
+      this.selectVideo = this.formatDataVideo(lista['items'][0])
+      console.log(this.selectVideo)
+      this.spinner.hide();
+    });
+  }
+
+  searchVideos() {
+    this.spinner.show();
+    this.videos = [];
+    this.youTubeService.searchVideos(this.search, 6).subscribe(lista => {
+      lista['items'].map(item => {
+        this.videos.push(this.formatDataVideo(item));
+      });
+      this.selectVideo = lista['items'][0]
+      this.spinner.hide();
+    });
+  }
+
+  formatDataVideo (video) {
+    return {
+      videoId: video.id.videoId,
+      description: video.snippet.description,
+      title: video.snippet.title,
+      thumbnails: video.snippet.thumbnails.medium,
+      urlEmbed: this.updateVideoUrl(video.id.videoId)
+    };
+  }
+
+  updateVideoUrl(id: string) {
+    const url = 'https://www.youtube.com/embed/' + id;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
--- src/app/youtube.service.ts
+++ src/app/youtube.service.ts
@@ -8,15 +8,23 @@ import {Observable} from 'rxjs';
 })
 export class YoutubeService {
 
-  apiKey:string = 'YOUR-APIKEY-YOUTUBE';
+  apiKey = 'AIzaSyCatkoYLjQ5hwN68hF4zBUWdP-pFIIyqBA';
 
   constructor(public http: HttpClient) { }
 
-  getVideosForChanel(channel, maxResults): Observable<Object> {
-    let url = 'https://www.googleapis.com/youtube/v3/search?key=' + this.apiKey + '&order=date&part=snippet &type=video,id&maxResults=' + 6
+  getVideos(maxResults): Observable<Object> {
+    const url = 'https://www.googleapis.com/youtube/v3/search?key=' + this.apiKey + '&order=date&part=snippet &type=video,id&maxResults=' + maxResults;
     return this.http.get(url)
       .pipe(map((res) => {
         return res;
-      }))
+      }));
+  }
+
+  searchVideos(search, maxResults): Observable<Object> {
+    const url = 'https://www.googleapis.com/youtube/v3/search?key=' + this.apiKey + '&q=' + search + '&part=snippet &type=video,id&maxResults=' + maxResults;
+    return this.http.get(url)
+      .pipe(map((res) => {
+        return res;
+      }));
   }
 }
